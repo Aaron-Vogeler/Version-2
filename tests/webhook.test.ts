2ddca3f56af8eb51bb9c2259c68bc14f39b52d40
Aaron-Vogeler <aaronmvogeler@gmail.com>
2025-12-04 15:20:20 -0500

    Update webhook.test.ts

--- tests/webhook.test.ts
+++ tests/webhook.test.ts
@@ -1,5 +1,5 @@
 /**
- * Basic webhook integration test test to redploy fly.io worker
+ * Basic webhook integration test test to redploy fly.io worker redeploy
  * Tests the webhook -> event -> DB flow
  */
 
